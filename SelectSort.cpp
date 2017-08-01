17f91d7c66e2c8dc47471abe213d3c5e4640354d
sharkfin <shark_fin@126.com>
2017-08-01 20:32:26 +0800

    improve

--- SelectSort.cpp
+++ SelectSort.cpp
@@ -3,7 +3,19 @@
 #define N 10
 main()
 {
-	int i,j,k,a[N];
+	int i,j,k,a[N],t;
 	for(i=0;i<N;i++)
 		scanf("%d", &a[i]);
+	for(j=0;j<N-1;j++)
+	{
+		k=j;
+		for(i=j+1;i<=N-1;i++)
+			if(a[k]>a[i])
+				k=i;
+		t=a[j];
+		a[j]=a[k];
+		a[k]=t;
+	} 
+	for(i=0;i<N;i++)
+		printf("%5d",a[i]);
 } 
